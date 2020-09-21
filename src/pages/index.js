a3686148e73dbad89f2ecb3d877520674e5a9b0e
Elaine <elaineaqs@gmail.com>
2020-09-21 18:04:26 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,22 +9,28 @@ const IndexPage = () => (
     <SEO title="Elaine Aquino's Links" />
     <ol>
         <li>
-            <a className="btn" href="https://calendly.com/elaineaquino/minicoaching">Free 15-min coaching sessions + content audit</a>
+            <a className="btn" href="https://calendly.com/elaineaquino/minicoaching">FREE 15-min coaching sessions + content audit</a>
         </li>
         <li>
-            <a className="btn" href="https://youtube.com/elaineaqs89">Watch the vlog</a>
+            <a className="btn" href="https://elaineaquino.com/coaching">1:1 Aligned Content Coaching</a>
         </li>
         <li>
-            <a className="btn" href="https://anchor.fm/elaineaquino">Listen to the podcast</a>
+            <a className="btn" href="https://elaineaquino.com/subscribe">Subscribe to the Spiritual Multipassionate Newsletter Community</a>
+        </li>
+            <a className="btn" href="https://gumroad.com/l/elaineaquino-ebook">Content Creation for Intentional Creators eBook (FREE when you join our newsletter</a>
         </li>
         <li>
-            <a className="btn" href="https://gumroad.com/l/elaineaquino-ebook">Digital Content Creation ebook</a>
+            <a className="btn" href="https://elaineaquino.com/blog">Read the EA blog</a>
         </li>
         <li>
-            <a className="btn" href="https://elaineaquino.com/subscribe">Subscribe to Weekly Mindset Mailing List</a>
+            <a className="btn" href="https://youtube.com/elaineaqs89">Watch the the Beautiful Things and Memories vlog</a>
         </li>
         <li>
-            <a className="btn" href="https://elaineaquino.com/blog">Read the blog</a>
+            <a className="btn" href="https://anchor.fm/elaineaquino">Listen to the Beautiful Things and Memories podcast</a>
+        </li>
+        <li>
+        <li>
+            <a className="btn" href="https://elaineaquino.com/">Elaine Aquino Official Site</a>
         </li>
     </ol>
   </Layout>
